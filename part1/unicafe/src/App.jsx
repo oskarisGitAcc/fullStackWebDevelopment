64207832ab13f2ec64189f38e53667c59cbc482a
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-09 17:10:53 +0200

    Task 1.9: unicafe step 4
    Fix also the way of counting the average by multiplying with 100.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -14,27 +14,40 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
   
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
   const increaseBad = () => setBad(bad + 1)
-  const total = good + neutral + bad
 
-  return (
+  if (total != 0) {
+    return (
+      <div>
+        <h1>give feedback</h1>
+        <Button handleClick={() => increaseGood()} text="good" />
+        <Button handleClick={() => increaseNeutral()} text="neutral" />
+        <Button handleClick={() => increaseBad()} text="bad" />
+        <h1>statistics</h1>
+        <Statistics type={"good"} value={good} />
+        <Statistics type={"neutral"} value={neutral} />
+        <Statistics type={"bad"} value={bad} />
+        <Statistics type={"all"} value={total} />
+        <Statistics type={"average"} value={(good - bad) / total} />
+        <Statistics type={"positive"} value={100 * good / total + " %"} />
+      </div>
+    )
+  } else {
+    return (
     <div>
       <h1>give feedback</h1>
       <Button handleClick={() => increaseGood()} text="good" />
       <Button handleClick={() => increaseNeutral()} text="neutral" />
       <Button handleClick={() => increaseBad()} text="bad" />
       <h1>statistics</h1>
-      <Statistics type={"good"} value={good} />
-      <Statistics type={"neutral"} value={neutral} />
-      <Statistics type={"bad"} value={bad} />
-      <Statistics type={"all"} value={total} />
-      <Statistics type={"average"} value={(good - bad) / total} />
-      <Statistics type={"positive"} value={good / total + " %"} />
+      <p>No feedback given</p>
     </div>
-  )
+    )
+  }
 }
 
 export default App
