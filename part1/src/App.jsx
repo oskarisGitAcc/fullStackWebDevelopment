3956d04a025a3eef711a5111eb6c54355a38093b
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-09 12:38:58 +0200

    Task 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,14 +4,20 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return (
     <div>
-      {props.parts.map((part, index) => (
-        <p key={index}>
-          {part} {props.exercises[index]}
-        </p>
-      ))}
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
     </div>
   )
 }
