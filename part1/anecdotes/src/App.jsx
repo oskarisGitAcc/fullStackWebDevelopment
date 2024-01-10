78c27b86e154bbfcf06a3abc39d5a4589a80385f
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-10 10:30:09 +0200

    Task 1.13*: anecdotes step2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -13,17 +13,24 @@ const App = () => {
   ];
    
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const getRandomAnecdote = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length);
     setSelected(randomIndex);
   };
 
+  const voteForAnecdote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
   return (
     <div>
-      <div>
-        {anecdotes[selected]}
-      </div>
+      <div>{anecdotes[selected]}</div>
+      <div>has {votes[selected]} votes</div>
+      <button onClick={voteForAnecdote}>vote</button>
       <button onClick={getRandomAnecdote}>next anecdote</button>
     </div>
   );
