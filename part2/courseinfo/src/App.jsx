ab50b78825948644f01f9d2286ca996ee0adbcca
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-10 11:46:05 +0200

    Tasks 2.2-2.3: Course information steps7-8

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -13,11 +13,14 @@ const Part = ({ partName, exercises }) => {
 };
 
 const Content = ({ parts }) => {
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} partName={part.name} exercises={part.exercises} />
       ))}
+      <b>Total number of exercises: {totalExercises}</b>
     </div>
   );
 };
