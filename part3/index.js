37cf5a82a0db6c54514687c853e30f86b6096edb
Oskari Pyykkönen <oskari.pyykkonen@gmail.com>
2024-01-23 13:05:34 +0200

    3.5 Phonebook backend step5

--- part3/index.js
+++ part3/index.js
@@ -27,7 +27,7 @@ let persons = [
 ];
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  res.json(persons);
 });
 
 app.get('/info', (req, res) => {
@@ -40,21 +40,47 @@ app.get('/info', (req, res) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
 
   if (person) {
     response.json(person)
   } else {
     response.status(404).end()
-  }
+  };
+});
+
+function generateUniqueId() {
+  return Math.floor(Math.random() * 1000000);
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  console.log("test");
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number is missing',
+    });
+  };
+
+  const person = {
+    id: generateUniqueId(),
+    name: body.name,
+    number: body.number
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
 
-  response.status(204).end()
+  response.status(204).end();
 });
 
 const PORT = 3001
